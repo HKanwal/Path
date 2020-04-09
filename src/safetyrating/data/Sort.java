974509d610e17ca92daaf6f8ee32bb5daa27e150
PradCoder <2010pesara@gmail.com>
2020-04-09 15:35:53 -0400

    Added initial sort

---
+++ src/safetyrating/data/Sort.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+package safetyrating.data;
+
+/**
+ * @author Pesara
+ *
+ */
+public class Sort {
+	
+	
+	//Sort the 273 
+	private static Comparable[] aux;
+	
+	public static void sort(Comparable[] a) {
+		aux = new Comparable[a.length];
+		sort(a,0,a.length-1);
+	}
+	
+	private static void sort(Comparable[] a, int lo, int hi) {
+		if(hi <= lo) return;
+		int mid = lo + (lo+hi)/2;
+		sort(a,lo,mid);
+		sort(a,mid+1,hi);
+		merge(a,lo,mid,hi);
+	}
+	//Abstract in-place merge page 271
+	private static void merge(Comparable[] a, int lo, int mid, int hi) {
+		int i = lo,j = mid+1;
+		
+		for(int k=lo; k <= hi; k++) {
+			aux[k] = a[k]; 
+		}
+		
+		for (int k = lo; k <= hi; k++) {
+			if(i>mid)  					  a[k] = aux[j++];
+			else if (j > hi) 			  a[k] = aux[i++];
+			else if(less(aux[j], aux[i])) a[k] = aux[j++];
+			else						  a[k] = aux[i++];
+			
+		}
+	}
+
+	private static boolean less(Comparable comparable, Comparable comparable2) {
+		int s = comparable.compareTo(comparable2);
+		if(s<0) {
+			return true;
+		}
+		return false;
+	}
+	
+}
