379630b7f4f0ace6871c9dc889599b69b9834236
PradCoder <2010pesara@gmail.com>
2020-04-10 13:04:58 -0400

    A lot of errors due to bad file arrangement just for initial API

--- demo/demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,20 @@
 package com.example.demo;
 
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +23,85 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+}
+
+@Component
+class UserCommandLineRunner implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		for(User b : this.userRepository.findAll()) {
+			System.out.println(b.toString());
+		}		
+	}
+	
+	@Autowired UserRepository userRepository;
+	
+
+}
+interface UserRepository extends JpaRepository<User, Long>{
+	
+	Collection<User> findByUsername(String userName);
+}
+
+@RestController
+class UserRestController {
+	
+
+	@RequestMapping("/User")
+	Collection<User> users (){
+		return this.userRepository.findAll();
+	}
+	
+	@Autowired UserRepository userRepository;
+}
+
+@Entity
+class User{
+	
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	private String userName;
+	private String password;
+	private String email;
+	
+	
+	public User(String userName, String password, String email) {
+		super();
+		this.userName = userName;
+		this.password = password;
+		this.email = email;
+	}
+
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", userName=" + userName + ", password=" + password + ", email=" + email + "]";
+	}
+
+	
 }
---
+++ demo/demo/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user(user_name,password,email) values('Kris','password','jenn@gmail.com');
+insert into user(user_name,password,email) values('Martin','password','jenn@gmail.com');
+insert into user(user_name,password,email) values('Andy','password','jenn@gmail.com');
+insert into user(user_name,password,email) values('Josh','password','jenn@gmail.com');
