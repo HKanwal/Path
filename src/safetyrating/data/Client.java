e5454b880e86320ad8d228ed1400f104dcb302c0
Harkeerat Kanwal <kanwal.harkeerat@gmail.com>
2020-04-11 20:54:29 -0400

    Added some comments.

--- src/safetyrating/data/Client.java
+++ src/safetyrating/data/Client.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 
 
 /**
- * Client to test CollisionDatabase.
+ * Client to test and use CollisionDatabase.
  * 
  * @author Harkeerat Kanwal
  */
 public class Client {
-
 	public static void main(String[] args) {
+		// Initialize database (parse data file).
 		CollisionDatabase data = null;
 		try {
 			data = new CollisionDatabase();
@@ -18,12 +18,14 @@ public class Client {
 		catch (IOException e) {
 			e.printStackTrace();
 		}
+
+		// Print some statistics upon parse completion.
 		System.out.println("Parsing complete.");
 		System.out.println("Number of entries: " + data.getNumEntries());
 		System.out.println("Total size of hash maps: " + data.getTotalSize());
 
 		QueryFactory factory = new QueryFactory(data);
-		factory.start();
+		factory.start(); // Run CLI
 	}
 
 }
